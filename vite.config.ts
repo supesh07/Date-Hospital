d18fef6370c018cb1320f47e7486f6c03826ea84
supesh07 <karangalesupesh5@gmail.com>
2026-07-03 11:54:46 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,9 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  return {
-    plugins: [react(), tailwindcss()],
+return {
+base:'/Date-Hospital/',
+  plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
