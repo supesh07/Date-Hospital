1bae836786d8780b604d3a1b379c9d0bc0844947
supesh07 <karangalesupesh5@gmail.com>
2026-07-03 12:01:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
 return {
-base:'/Date-Hospital/',
+base:,'Date-Hospital',
   plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
